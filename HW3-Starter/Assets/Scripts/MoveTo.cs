427da4efdae2a0ea406184875ce93f478b6aa506
NarraAtor <eff3587@rit.edu>
2023-03-04 00:08:28 -0500

    Added HW 3 starter code

---
+++ HW3-Starter/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+namespace BehaviorTree
+{
+    [Serializable]
+    public class MoveTo : Task
+    {
+        // The character that is moving to a different location.
+        private Character mover;
+        public Character Mover
+        {
+            get { return mover; }
+            set { mover = value; }
+        }
+
+        // The location the character is moving to.
+        private Location where;
+        public Location Where
+        {
+            get { return where; }
+            set { where = value; }
+        }
+
+        // This constructor defaults the character and location to None.
+        public MoveTo() : this(Character.None, Location.None) { }
+
+        // This constructor takes parameters for the character and location.
+        public MoveTo (Character mover, Location where)
+        {
+            Mover = mover;
+            Where = where;
+        }
+
+        // This method runs the MoveTo action on the given WorldState object.
+        public override bool run (WorldState state)
+        {
+            // Fill in your conditional logic here.
+            if (state.Debug) Debug.Log(this + " Success");
+            return true;
+
+            if (state.Debug) Debug.Log(this + " Fail");
+            return false;
+        }
+
+        // Creates and returns a string describing the MoveTo action.
+        public override string ToString()
+        {
+            return mover + " moves to " + where;
+        }
+    }
+}
